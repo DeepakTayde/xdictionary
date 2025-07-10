a2a553608033e637a2cbc2c8287f40a3853ba404
DeepakTayde <deepaktayde559@gmail.com>
2025-07-10 13:31:50 +0530

    first commit and initial setup of xdictionary

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import XDictionary from './XDictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <XDictionary/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
